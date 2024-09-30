997d26886782f3461823fc50dcb83be6915f54ff
Trần Trung <trantrung@Trans-MacBook-Pro.local>
2024-09-30 18:09:25 +0700

    trungtran - Updated link github

--- src/component/Footer/index.jsx
+++ src/component/Footer/index.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import "./style.scss";
+import { Link } from "react-router-dom/cjs/react-router-dom";
 
 function Footer() {
     return (
         <div className="footer">
-            <p className="footer__text">Copyright © Pokemon | by Trungtrungcum</p>
+            <p className="footer__text">
+                Copyright © Pokemon | by{" "}
+                <Link to="https://github.com/trungtrung-art/pokemon-app-react">Trungtrungcum</Link>
+            </p>
         </div>
     );
 }
--- src/component/Header/index.jsx
+++ src/component/Header/index.jsx
@@ -2,10 +2,14 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./style.scss";
 
+import logoGithub from "../../assets/image/logo-github.png";
+
 function Header({ type, id }) {
     return (
         <div className={`header__container header__container--${type === "" || type === "all" ? "white" : type}`}>
-            <Link to={`/pokemon/all`}>{id && <i className="fas fa-arrow-left text-white"></i>}</Link>
+            <a href="https://github.com/trungtrung-art/pokemon-app-react" target="_blank" className="link-github">
+                <img src={logoGithub} className="logo-github" />
+            </a>
             <header className={`header__title header__title--${type}`}>PokéDex</header>
         </div>
     );
